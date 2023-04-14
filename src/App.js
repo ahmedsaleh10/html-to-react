8b4c9666ff0a67270877c1a299ed560875e21323
Ahmed <ahmed.ovicsaleh@outlook.com>
2023-04-14 18:10:30 +0300

    Convert Html,JS to React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [textArea, setTextArea] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [result, setResult] = useState("");
+
+  const handleOnChange = (e) => {
+    setTextArea(e.target.value);
+  };
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    setResult("");
+    try {
+      await submitForm(textArea);
+      setResult("success");
+    } catch (err) {
+      setResult(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  function submitForm(answer) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (answer.toLowerCase() === "istanbul") {
+          resolve();
+        } else {
+          reject(new Error("Good guess but a wrong answer. Try again!"));
+        }
+      }, 1500);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {result !== "success" ? (
+        <form id="form" onSubmit={handleFormSubmit}>
+          <h2>City quiz</h2>
+          <p>What city is located on two continents?</p>
+          <textarea
+            id="textarea"
+            onChange={handleOnChange}
+            disabled={isLoading}
+          ></textarea>
+          <br />
+          <button id="button" disabled={!textArea.length || isLoading}>
+            Submit
+          </button>
+          {isLoading && <p id="loading">Loading...</p>}
+
+          <p id="error" style={{ color: "red" }}>
+            {result}
+          </p>
+        </form>
+      ) : (
+        <h1 id="success">That's right!</h1>
+      )}
     </div>
   );
 }
